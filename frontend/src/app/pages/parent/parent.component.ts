771c200963b82d1336bbc49eef7ece71f7690ce8
Nikitkov N <User@LENOVO>
2024-02-01 11:18:18 +0300

    angular new components

---
+++ frontend/src/app/pages/parent/parent.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { NavigationComponent } from '../../components/navigation/navigation.component'; 
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router'; 
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.css'
+})
+export class ParentComponent {
+
+}
